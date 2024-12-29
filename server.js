2423068cf03b7630082ef4aa088d7d4d5612a408
Priyansu Rout <priyansurout192004@gmail.com>
2024-12-29 21:24:43 +0530

    reset the cors

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const {
 require("dotenv").config();
 
 
-// const corsOptions = {
-//   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-//   credentials: true,
-//   optionsSuccessStatus: 200
-// };
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200
+};
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 
 
 
